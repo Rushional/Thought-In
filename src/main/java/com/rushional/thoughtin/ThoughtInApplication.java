649e8dba1f0b9ec1a15b5177802e139ae3268244
Rushional <Rushional2@gmail.com>
2021-08-22 19:22:20 +0200

    first commit

---
+++ src/main/java/com/rushional/thoughtin/ThoughtInApplication.java
@@ -0,0 +1,13 @@
+package com.rushional.thoughtin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThoughtInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rushional/thoughtin/ThoughtInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushional.thoughtin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
